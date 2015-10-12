a2ddbcaf0089a0f4ecbfaea4edb32ba800bd318f
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2015-10-12 13:59:56 +0200

    callback

--- callback3.js
+++ callback3.js
@@ -1,25 +1,10 @@
 var fs = require('fs');
 
-// sync
-var file = fs.readFileSync('alma');
-console.log(file.toString());
 
-
-// async
-
-fs.readFile('alma', function(err, file) {
-  console.log(file.toString());
+fs.readFile('korte', function(err, f) {
+  console.log(f.toString());
+});
+fs.readFile('alma', function(err, f) {
+  console.log(f.toString());
 });
 
-function getAlmaContentAndKorte(cb) {
-  fs.readFile('alma', function(err, file) {
-    var output = '';
-    output += file.toString();
-    output += 'korte';
-    return cb(err, output);
-  });
-}
-
-getAlmaContentAndKorte(function(err, content) {
-  console.log(content);
-});
---
+++ module.js
@@ -0,0 +1,10 @@
+var alma = 1;
+var korte = 2;
+var dinnye = 3;
+
+
+module.exports = {
+  alma: alma,
+  korte: korte,
+  dinnye: dinnye
+};
